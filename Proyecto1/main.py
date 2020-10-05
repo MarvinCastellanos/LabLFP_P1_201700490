aef03698be6ce3d6392e54df8f4cf0a618be318c
Marvin Castellanos <madacast2@gmail.com>
2020-10-04 19:28:42 -0600

    print in

--- Proyecto1/main.py
+++ Proyecto1/main.py
@@ -1,6 +1,12 @@
+import sys
+from termcolor import colored, cprint
+
+
 set={}
 comandos=[]
+tokensEncontrados=[]
 palabraUsada=''
+color='white'
 
 
 letras=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","-",
@@ -161,18 +167,19 @@ def sqli(texto):
             if numero==caracter:
                 palComandoAux+=caracter
                 break
-        if caracter=='*' or caracter=='=' or caracter=='!' or caracter=='>' or caracter =='<':
-            palComandoAux+=caracter
-        if caracter==',' or caracter==' ' or caracter=='=' or caracter==';':
-            if not palComandoAux=='':
+        if caracter == '*' or caracter == '=' or caracter=='!' or caracter == '>' or caracter == '<':
+            palComandoAux += caracter
+        if caracter == ',' or caracter == ' ' or caracter == '=' or caracter == ';':
+            if not palComandoAux == '':
                 comando.append(palComandoAux)
-                palComandoAux=''
-        if caracter==';':
+                palComandoAux = ''
+        if caracter == ';':
             comandos.append(comando)
-            comando=[]
+            comando = []
+
 
 com=[]
-#separa las palabras del comando ingresado y las ingresa en un vector
+# separa las palabras del comando ingresado y las ingresa en un vector
 def instruccion(texto):
 
     palComandoAux = ''
@@ -202,7 +209,7 @@ def instruccion(texto):
 def principal():
     texto=''
     while True:
-        print('Bienvenido, ingrese su comando: ')
+        print(colored('Bienvenido, ingrese su comando: ',color))
         texto=input()
         instruccion(texto)
         #print(com)
@@ -216,6 +223,7 @@ def verificaComando(vector):
     global set
     global setID
     global palabraUsada
+    global color
     estado=0
     for palabra in vector:
 #create set
@@ -227,7 +235,7 @@ def verificaComando(vector):
             continue
         if estado==2:
             set[palabra]=[]
-            print('Se ha creado el ID ',palabra)
+            print(colored(('Se ha creado el ID '+palabra),color))
             estado=0
             continue
 #load into
@@ -243,7 +251,7 @@ def verificaComando(vector):
             continue
         if estado==5:
             automataAtrib(palabra)
-            print('Se ha cargado '+palabra+' en '+setID)
+            print(colored('Se ha cargado '+palabra+' en '+setID,color) )
             continue
 #use set
         if estado==0 and palabra.lower()=='use':
@@ -256,10 +264,10 @@ def verificaComando(vector):
             if palabra in set:
                 palabraUsada=palabra
                 estado=0
-                print('Ahora esta utilizando el ID '+palabraUsada)
+                print(colored('Ahora esta utilizando el ID '+palabraUsada, color))
                 continue
             else:
-                print('El ID '+ palabra+' no se encuentra registrado')
+                print(colored('El ID '+ palabra+' no se encuentra registrado',color))
                 estado=0
                 continue
 #list atributes
@@ -267,9 +275,9 @@ def verificaComando(vector):
             estado=8
             continue
         if estado==8 and palabra.lower()=='atributes':
-            print(palabraUsada+':')
+            print(colored(palabraUsada+':',color))
             for atrib in set[palabraUsada][0]:
-                print(atrib)
+                print(colored(atrib,color))
             estado=0
             continue
 #max
@@ -278,11 +286,11 @@ def verificaComando(vector):
             continue
         if estado==9:
             maximo=-5000000.0
-            print(palabraUsada+', '+palabra+':')
+            print(colored(palabraUsada+', '+palabra+':'))
             for max in set[palabraUsada]:
                 if float(max[palabra]) >= maximo:
                     maximo=float(max[palabra])
-            print (maximo)
+            print (colored(maximo,color))
             estado=0
             continue
 #min
@@ -291,11 +299,11 @@ def verificaComando(vector):
             continue
         if estado==10:
             minimo=50000000.0
-            print(palabraUsada + ', ' + palabra + ':')
+            print(colored(palabraUsada + ', ' + palabra + ':',color))
             for min in set[palabraUsada]:
                 if float(min[palabra]) <= minimo:
                     minimo=float(min[palabra])
-            print (minimo)
+            print (colored(minimo,color))
             estado=0
             continue
 #sum
@@ -304,10 +312,10 @@ def verificaComando(vector):
             continue
         if estado==11:
             suma=0.0
-            print(palabraUsada + ', ' + palabra + ':')
+            print(colored(palabraUsada + ', ' + palabra + ':',color))
             for sum in set[palabraUsada]:
                 suma+=float(sum[palabra])
-            print(suma)
+            print(colored(suma),color)
             continue
 #count
         if estado==0 and palabra.lower()=='count':
@@ -315,22 +323,22 @@ def verificaComando(vector):
             continue
         if estado==12:
             conteo=0
-            print(palabraUsada + ', ' + palabra + ':')
+            print(colored(palabraUsada + ', ' + palabra + ':',color))
             for cont in set[palabraUsada]:
                 if not cont[palabra]=='null':
                     conteo+=1
-            print(conteo)
+            print(colored(conteo,color))
             continue
 #select
         if estado==0 and palabra.lower()=='select':
             estado=13
             continue
         if estado==13 and palabra=='*' and len(vector)==2:
-            print(palabraUsada+':\n<<<<<<<<<<<<<<<<<<<<<<')
+            print(colored(palabraUsada+':\n<<<<<<<<<<<<<<<<<<<<<<',color))
             for atributos in set[palabraUsada]:
                 for atributo in atributos:
                     print(atributo+':'+atributos[atributo])
-                print('-----------------------')
+                print(colored('-----------------------'),color)
             continue
         if estado==13 and palabra=='*':
             estado=14
@@ -349,12 +357,12 @@ def verificaComando(vector):
         if estado==17:
             selecAtribVal=''
             selecAtribVal=palabra
-            print(palabraUsada+':\n<<<<<<<<<<<<<<<<<<<<<')
+            print(colored(palabraUsada+':\n<<<<<<<<<<<<<<<<<<<<<',color))
             for atrib in set[palabraUsada]:
                 if atrib[selecAtrib]==selecAtribVal:
                     for pal in atrib:
-                        print(pal+': '+atrib[pal])
-                    print('------------------------')
+                        print(colored(pal+': '+atrib[pal],color))
+                    print(colored('------------------------',color))
             continue
 #script
         if estado==0 and palabra.lower()=='script':
@@ -367,7 +375,16 @@ def verificaComando(vector):
             sqli(entrada)
             for comando in comandos:
                 verificaComando(comando)
-                print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+                print(colored('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%',color))
             file.close()
+#print in
+        if estado==0 and palabra.lower()=='print':
+            estado=19
+            continue
+        if estado==19 and palabra.lower()=='in':
+            estado=20
+            continue
+        if estado==20:
+            color=palabra
 
 principal()
